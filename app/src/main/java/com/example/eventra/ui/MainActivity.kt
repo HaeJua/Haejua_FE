f30abf72377b54254d3a862208bbec0dac6f896d
hbohhh <develop31@naver.com>
2025-05-28 17:26:49 +0900

    your message

--- app/src/main/java/com/example/eventra/ui/MainActivity.kt
+++ app/src/main/java/com/example/eventra/ui/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.example.eventra.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.eventra.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val openDetailButton = findViewById<Button>(R.id.openDetailButton)
+
+        openDetailButton.setOnClickListener {
+            val intent = Intent(this, detail_hb::class.java)
+            startActivity(intent)
         }
     }
 }
